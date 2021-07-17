06b9d6c20d9c5bb4e85b62a48586a15e412de8b7
aagyeya <arnav_agyeya@mentor.com>
2021-07-17 20:32:58 +0530

    ConfigServerApplication

--- Configuration/src/main/java/com/example/configuration/ConfigurationService.java
+++ Configuration/src/main/java/com/example/configuration/ConfigurationService.java
@@ -1,2 +1,33 @@
-package com.example.configuration;public class ConfigurationService {
+package com.example.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/")
+public class ConfigurationService {
+
+    @Autowired
+    DBSettings dbSettings;
+
+    @Autowired
+    Environment environment;
+
+    @RequestMapping(path = "/dbsettings",method = RequestMethod.GET)
+    public String getDbSettings(){
+        return dbSettings.getConnection()
+               + "##"
+               + dbSettings.getHost()
+               + "##"
+               + dbSettings.getPort();
+    }
+
+    @GetMapping(path = "/getprofile")
+    public String getProfileDetails(){
+        return environment.toString();
+    }
 }
